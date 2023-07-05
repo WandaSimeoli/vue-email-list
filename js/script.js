ad3713ac38757c1939d2ed7fff9e1ab0eb0bd41e
WandaSimeoli <wandasimeoli@gmail.com>
2023-07-05 18:51:27 +0200

    Ciclo for di email causali

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello World!'
+        
       }
+    },
+    methods : {
+
     }
   }).mount('#app')
+
+  console.log(axios);
+  
+  for (let index = 0; index < 10; index++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function(response) {
+      const result = response.data;
+      console.log(result.response);
+    });
+
+  }
+
+ 
